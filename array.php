23676ed4222ca864724a2c6d22a176b1cbe3c993
Md Hafiz Al Foisal <alfoisal940700@gmail.com>
2023-12-20 23:04:28 +0600

    initial commit

---
+++ array.php
@@ -0,0 +1,25 @@
+<?php
+
+// $age = [
+//     '1' => ['25', '26'],
+//     '2',
+//     '3',
+//     '7',
+//     '5',
+// ];
+$person = [
+    'name'    => 'Mr. Abdul Karim',
+    'phone'   => '017896523',
+    'age'     => 30,
+    'address' => [
+        'home' => 5,
+        'road' => 10,
+        'line' => [
+            'line_1' => 'Road side',
+            'line_2' => 'Road outside',
+        ],
+    ],
+];
+echo '<pre>';
+print_r($person['address']['line']['line_2']);
+echo '</pre>';
---
+++ index.php
@@ -0,0 +1,42 @@
+<?php
+
+//single comment
+
+/**
+ * multiline comment
+ */
+$age = "18";
+
+$foo = 5;
+
+// int string double
+
+// if ($age > 18) {
+
+//     echo 'Grater than 18 years old';
+
+// }
+
+const AGE = 18;
+
+// if (AGE == 18) {
+
+//     echo 'Age equal to 18<br>';
+//     var_dump($age);
+
+// } else {
+//     echo 'Less than 18 years old<br>';
+//     var_dump($age);
+
+// }
+
+
+echo AGE;
+
+// (condition)?'':'';
+
+// echo $age == 18 ? 'Age equal to 18' : 'Less than 18 years old';
+
+// echo '<br> Global: ' . $foo;
+
+?>
